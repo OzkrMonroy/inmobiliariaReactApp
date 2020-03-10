799e7df933d1238fd17214ea6dabd8f73a9a81d5
Oscar Monroy <ozkrmonroy65@gmail.com>
2020-03-09 19:51:34 -0600

    InmueblesListAction.js file has been created

---
+++ src/session/actions/InmueblesListAction.js
@@ -0,0 +1,46 @@
+export const getData = (firebase, pageSize, initialHouse, textSearch) => {
+  return new Promise(async (resolve, reject) => {
+    let houses = firebase.db
+                 .collection("Homes")
+                 .orderBy("address")
+                 .limit(pageSize)
+
+    if(initialHouse !== null) {
+      houses = firebase.db
+               .collection("Homes")
+               .orderBy("address")
+               .startAfter(initialHouse)
+               .limit(pageSize)
+
+      if(textSearch.trim() !== "") {
+        houses = firebase.db
+                 .collection("Homes")
+                 .orderBy("address")
+                 .where("keywords", "array-contains", textSearch.toLowerCase())
+                 .startAfter(initialHouse)
+                 .limit(pageSize)
+      }
+    }
+
+    const snapshot = await houses.get()
+
+    const housesArray = snapshot.docs.map(doc => {
+      let data = doc.data()
+      let id = doc.id
+
+      return {id, ...data}
+    })
+
+    const firstHouse = snapshot.docs[0]
+    const lastHouse = snapshot.docs[snapshot.docs.length -1]
+
+    const returnValue = {
+      housesArray,
+      firstHouse,
+      lastHouse
+    }
+
+    resolve(returnValue)
+  })
+
+}
